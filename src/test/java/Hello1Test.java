e7aa4e478b0e2c264ff23a99e78c1c179a6308b2
Zeynep-bak <zeynepsimsek81@mail.com>
2021-06-25 13:51:40 +0200

    added second update

--- src/test/java/Hello1Test.java
+++ src/test/java/Hello1Test.java
@@ -2,5 +2,6 @@ public class Hello1Test {
 
     public static void main(String[] args) {
         System.out.println("Hello1");
+        System.out.println("About to share project on Github");
     }
 }
